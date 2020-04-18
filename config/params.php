7c210bdd4a9ca123f244d48213a921073acd6f41
shigo <shigoakin@gmail.com>
2020-04-18 11:47:00 +0100

    Add files via upload

---
+++ config/params.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'adminEmail' => 'akin@olusegun.com.ng',
+    'senderEmail' => 'noreply@olusegun.com.ng',
+    'senderName' => 'TryIT Traning mailer',
+];
---
+++ config/web.php
@@ -0,0 +1,91 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'name' => 'Try IT Training',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'layoutPath' => '@app/views/layouts',
+    'layout' => 'newmain.php',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'qMviwq8PFOeXKJUWQow78Dzc7h33SGfP',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+    'class' => 'yii\swiftmailer\Mailer',
+    //'viewPath' => '@common/mail',
+    'useFileTransport' => false,
+    'transport' => [
+        'class' => 'Swift_SmtpTransport',
+        'host' => gethostbyname('smtp.gmail.com'),
+        'username' => 'shigoakin@gmail.com',
+        'password' => 'Akinman@1975',
+        'port' => '465',
+        'encryption' => 'ssl',
+        'streamOptions' => [ 
+            'ssl' => [ 
+                'allow_self_signed' => true,
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ],
+        ]
+    ]
+],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+        
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
